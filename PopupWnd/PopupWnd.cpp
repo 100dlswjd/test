796d36ca65557c0d1ee7dcf71ec05f95be9da702
100dlswjd <82484962+100dlswjd@users.noreply.github.com>
2021-08-20 22:05:49 +0900

    popupwindow

---
+++ PopupWnd/PopupWnd.cpp
@@ -0,0 +1,60 @@
+#include<windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+HWND hWndMain;
+LPCTSTR lpszClass = TEXT("팝업윈도우");
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow) {
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(LTGRAY_BRUSH);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_POPUPWINDOW,10,10,200,100, NULL, (HMENU)NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+	hWndMain = hWnd;
+
+	while (GetMessage(&Message, NULL, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return (int)Message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
+	HDC hdc;
+	PAINTSTRUCT ps;
+	CONST TCHAR* Mes = TEXT("팝업 윈도우입니다");
+	UINT nHit;
+
+	switch (iMessage) {
+		hdc = BeginPaint(hWnd, &ps);
+		SetBkMode(hdc, TRANSPARENT);
+		TextOut(hdc, 10, 10, Mes, lstrlen(Mes));
+		EndPaint(hWnd, &ps);
+		return 0;
+	case WM_NCHITTEST:
+		nHit = DefWindowProc(hWnd, iMessage, wParam, lParam);
+		if (nHit == HTCLIENT)
+			nHit = HTCAPTION;
+		return nHit;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
+
