b5333eefec6a5f4d6122bfa57c2d1da7dc5c5fe0
100dlswjd <82484962+100dlswjd@users.noreply.github.com>
2021-08-26 20:08:17 +0900

    ScreenToClient

---
+++ ScreenToClient/ScreenToClient.cpp
@@ -0,0 +1,63 @@
+#include<windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+HWND hWndMain;
+LPCTSTR lpszClass = TEXT("Class");
+HWND hBtn;
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow) {
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(COLOR_WINDOW + 1);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, (HMENU)NULL, hInstance, NULL);
+
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, NULL, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return (int)Message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
+	HDC hdc;
+	PAINTSTRUCT ps;
+	RECT brt;
+
+	switch (iMessage) {
+	case WM_CREATE:
+		hWndMain = hWnd;
+		hBtn = CreateWindow(TEXT("button"), TEXT("Å×½ºÆ®"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 168, 125, 96, 42, hWnd, (HMENU)0, g_hInst, NULL);
+		return 0;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		GetWindowRect(hBtn, &brt);
+		InflateRect(&brt, 50, 50);
+		ScreenToClient(hWnd, (LPPOINT)&brt);
+		ScreenToClient(hWnd, (LPPOINT)&brt.right);
+		Rectangle(hdc, brt.left, brt.top, brt.right, brt.bottom);
+		EndPaint(hWnd, &ps);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
+
