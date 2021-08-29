25f26c2f7f032a2cab3e8305b8cc796c1a798a3f
100dlswjd <82484962+100dlswjd@users.noreply.github.com>
2021-08-29 14:25:40 +0900

    MiniSpy

--- MiniSpy/MiniSpy.cpp
+++ MiniSpy/MiniSpy.cpp
@@ -42,7 +42,42 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 	static HWND hWndOld;
 	static TCHAR str[256];
 	TCHAR sCaption[256], sClass[256];
+	RECT crt;
 
+	switch (iMessage) {
+	case WM_CREATE:
+		SetTimer(hWnd, 1, 500, NULL);
+		return 0;
+	case WM_TIMER:
+		GetCursorPos(&pt);
+		hWndPoint = WindowFromPoint(pt);
+		if (hWndPoint == hWndOld) {
+			return 0;
+		}
+		hWndOld = hWndPoint;
+		if (hWndPoint == NULL) {
+			lstrcpy(str, TEXT("윈도우 없음"));
+		}
+		else {
+			GetWindowText(hWndPoint, sCaption, 256);
+			GetClassName(hWndPoint, sClass, 256);
+			hWndParent = GetParent(hWndPoint);
+			GetWindowRect(hWndPoint, &crt);
+			wsprintf(str, TEXT("핸들 : %d(0x%x)\r\n 클래스 : %s\r\n 캡션 : %s\r\n")TEXT("Parent : %d(0x%x)\r\n")TEXT("Rect : (%d,%d) - (%d,%d), %d*%d"),
+				hWndPoint, hWndPoint, sClass, sCaption, hWndParent, hWndParent, crt.left, crt.top, crt.right, crt.bottom, crt.right - crt.left, crt.bottom - crt.top);
+		}
+		InvalidateRect(hWnd, NULL, TRUE);
+		return 0;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		GetClientRect(hWnd, &crt);
+		DrawText(hdc, str, -1, &crt, 0);
+		EndPaint(hWnd, &ps);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
 	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
 }
 
