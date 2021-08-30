1250ae31d535f934a4a1d41ba3b25eb804d36d8d
100dlswjd <82484962+100dlswjd@users.noreply.github.com>
2021-08-30 22:12:46 +0900

    WmSize

---
+++ WmSize/WmSize.cpp
@@ -0,0 +1,66 @@
+#include<windows.h>
+
+#define ID_LISTBOX 100
+
+CONST TCHAR* Items[] = { TEXT("Apple"),TEXT("Orange"),TEXT("Melon"),TEXT("Grape"),TEXT("Strawberry") };
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+HWND hWndMain;
+HWND hList;
+LPCTSTR lpszClass = TEXT("WmSize");
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow) {
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(COLOR_WINDOW + 1);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, (HMENU)NULL, hInstance, NULL);
+
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, NULL, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return (int)Message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
+	int i;
+	RECT crt;
+
+	switch (iMessage) {
+	case WM_CREATE:
+		hList = CreateWindow(TEXT("listbox"), NULL, WS_CHILD | WS_VISIBLE | LBS_NOINTEGRALHEIGHT, 0, 0, 0, 0, hWnd, (HMENU)ID_LISTBOX, g_hInst, NULL);
+		for (i = 0; i < 5; i++) {
+			SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)Items[i]);
+		}
+		return 0;
+	case WM_SIZE:
+		GetClientRect(hWnd, &crt);
+		MoveWindow(hList, crt.left, crt.top, crt.right, crt.bottom, TRUE);
+		return 0;
+	case WM_SIZING:
+		if (wParam == WMSZ_LEFT) {
+			((RECT*)lParam)->left = ((RECT*)lParam)->left / 50 * 50;
+			return TRUE;
+		}
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
