14bfefdd33d8d58294cc8e5d527faf541511ae2a
100dlswjd <100dlswjd@naver.com>
2021-09-24 21:53:22 +0900

    SendCallback

---
+++ SendCallback/SendCallback.cpp
@@ -0,0 +1,67 @@
+#include<windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+HWND hWndMain;
+LPCTSTR lpszClass = TEXT("SendCallback");
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow) {
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(COLOR_WINDOW + 1);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, (HMENU)NULL, hInstance, NULL);
+
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, NULL, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return (int)Message.wParam;
+}
+
+VOID CALLBACK SendAsyncProc(HWND hWnd, UINT uMsg, ULONG_PTR dwData, LRESULT lResult ) {
+	MessageBox(hWndMain, TEXT("작업을 완료했습니다."), TEXT("Send1"), MB_OK);
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
+	HDC hdc;
+	PAINTSTRUCT ps;
+	HWND hSend2;
+	CONST TCHAR* Mes = TEXT("왼쪽 버튼을 누르시오");
+
+	switch (iMessage) {
+	case WM_LBUTTONDOWN:
+		hSend2 = FindWindow(NULL, TEXT("Send2"));
+		if (hSend2 != NULL) {
+			SendMessageCallback(hSend2, WM_USER + 1, 0, 0, SendAsyncProc, NULL);
+		}
+		return 0;
+	case WM_CREATE:
+		return 0;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		TextOut(hdc, 10, 10, Mes, lstrlen(Mes));
+		EndPaint(hWnd, &ps);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
+
