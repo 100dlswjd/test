d844641cf0fbe3f759c09eb45e41efba19d0f677
100dlswjd <82484962+100dlswjd@users.noreply.github.com>
2021-09-25 15:04:34 +0900

    autoKeybd

--- autoClick/autoClick.cpp
+++ autoClick/autoClick.cpp
@@ -4,6 +4,7 @@ LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 HINSTANCE g_hInst;
 HWND hWndMain;
 LPCTSTR lpszClass = TEXT("autoClick");
+int count = 0;
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow) {
 	HWND hWnd;
@@ -33,26 +34,40 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmd
 	}
 	return (int)Message.wParam;
 }
-
+	
 LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
 	HDC hdc;
 	PAINTSTRUCT ps;
 	POINT ptMouse;
 
-	GetCursorPos(&ptMouse);
-
 	switch (iMessage) {
 	case WM_HOTKEY:
+		if (wParam == 0x0001) {
+			SetTimer(hWnd, 0x0001, 10, NULL);
+		}
+		else {
+			KillTimer(hWnd, 0x0001);
+		}
+		/*GetCursorPos(&ptMouse);
 		mouse_event(0x0002, ptMouse.x, ptMouse.y, 0, 0);
-		mouse_event(0x0004, ptMouse.x, ptMouse.y, 0, 0);
+		mouse_event(0x0004, ptMouse.x, ptMouse.y, 0, 0);*/
+		return 0;
+	case WM_TIMER:
+		GetCursorPos(&ptMouse);
+		mouse_event(0x0002, ptMouse.x, ptMouse.y, 0, 0);
+		mouse_event(0x0004, ptMouse.x, ptMouse.y, 0, 0); 
+		// 메시지 처리 순서, 타이머 페인트 핫키 
 		return 0;
 	case WM_CREATE:
-		RegisterHotKey(hWnd,0x0001,0, VK_LEFT);
+		RegisterHotKey(hWnd, 0x0001, 0, VK_LEFT);
+		RegisterHotKey(hWnd, 0x0002, 0, VK_RIGHT);
 		return 0;
 	case WM_PAINT:
 		hdc = BeginPaint(hWnd, &ps);
 		return 0;	
 	case WM_DESTROY:
+		UnregisterHotKey(hWnd, 0x0001);
+		UnregisterHotKey(hWnd, 0x0002);
 		PostQuitMessage(0);
 		return 0;
 	}
